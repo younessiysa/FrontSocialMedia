407ba57eb466ec8f43848c0c2c1e68747d8bbd1c
younessiysa <youness.iysa@gmail.com>
2021-10-23 23:07:04 +0100

    first commit

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {}
+
+  login(): void {
+    this.accountService.login(this.model).subscribe((response) => {
+      this.router.navigateByUrl('/members');
+    });
+  }
+
+  logout(): void {
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
+}
